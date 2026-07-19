0b1007d46329682b47fb63a672acc1e089bcd843
Navbiga <navbiga1234@gmail.com>
2026-07-19 14:19:37 +0200

    rook, pawnand horse done

--- functions.py
+++ functions.py
@@ -55,12 +55,12 @@ def move(piece, square, target_square, board, turn):
                 return True
                 
         # Check if the pawn didnt promote
-    
+
+        return False # if nothing works the move is not legal
 
     # Knight Movement
     elif piece.piece == 'knight':
 
-            
         
         # two rows up and  column
         if abs(int(square[1]) - int(target_square[1])) == 2 and abs(ord(square[0]) - ord(target_square[0])) == 1:
@@ -69,10 +69,62 @@ def move(piece, square, target_square, board, turn):
             piece.move(target_square)
             return True
 
+        # two collums and one row
         if abs(int(square[1]) - int(target_square[1])) == 1 and abs(ord(square[0]) - ord(target_square[0])) == 2:
             if board[target_square] is not None:
                 piece.capture(target_square)
             piece.move(target_square)
             return True
 
-    return False
+        return False # if nothing works the move is not legal
+    
+    # Rook Movement
+    elif piece.piece == 'rook':
+        rook_check = True
+
+        # Logic and the same row
+        if square[1] == target_square[1]:
+
+            # Make a variable suitible for the range() function
+            start_ascii = min(ord(square[0]), ord(target_square[0]))
+            end_ascii = max(ord(square[0]), ord(target_square[0]))
+
+            for current_col in range(start_ascii + 1, end_ascii):
+                if rook_check and board[chr(current_col) + square[1]] is None:
+                    rook_check = True
+                else:
+                    rook_check = False
+                    return False
+
+            if rook_check:
+                # Success no obstacles in the way
+                if board[target_square] is not None:
+                    piece.capture(target_square)
+                piece.move(target_square)
+                return True
+
+
+        # Logic in the same collum SO MUCH EASIER AHH
+        elif square[0] == target_square[0]:
+
+            # numbers suitable for range()
+            start_int = min(int(square[1]), int(target_square[1]))
+            end_int = max(int(square[1]), int(target_square[1]))
+
+            for current_row in range(start_int + 1, end_int):
+                if rook_check and board[square[0] + str(current_row)] is None:
+                    rook_check = True
+                else:
+                    rook_check = False
+                    return False
+            
+            if rook_check:    
+                # Success no obstacles in the way
+                if board[target_square] is not None:
+                    piece.capture(target_square)
+                piece.move(target_square)
+                return True
+        
+        # If none of  these work then the move is ilegal
+        return False
+
